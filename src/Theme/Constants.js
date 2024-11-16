0d35c18762a796028977b94498525b225e32f3b9
Rakshit Patel <rakshit17p@gmail.com>
2024-11-15 16:22:51 -0800

    code restructure

---
+++ src/Theme/Constants.js
@@ -0,0 +1,10 @@
+export const CONSTANTS = {
+  COLORS: {
+    PrimaryBackground: "#f5f5f5",
+    MenuIcon: "#333",
+    WHITE: "#fff",
+    GREY: "#555",
+    DARKGREY: "#1f1f1f",
+    LightGrey: "#e0e0e0",
+  },
+};
